5991e5551a9a3631a87166d36816851d89927706
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-05 19:47:24 -0700

    added rental router to server

--- properties/propRouter.js
+++ properties/propRouter.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const knex = require('knex');
-const db = require('../database/dbConfig')
-const server =express();
+const db = require('../database/dbConfig');
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-const router =express.Router();
+const router = express.Router();
 
-router.get('/', async (req,res) => {
-    try{
-        const props = await db('properties')
-        res.json(props);
-    }catch(err){
-        res.status(500).json({message: 'Failed to retrieve properties'})
-    }
-})
+router.get('/', async (req, res) => {
+  try {
+    const props = await db('properties');
+    res.json(props);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to retrieve properties' });
+  }
+});
 
-
-
-
-
-module.exports =router;
+module.exports = router;
