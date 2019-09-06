db4b4dbb173f5fa15c2ae5c9cae7cf0cf2c31bd1
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-05 20:11:50 -0700

    updated rental routers

--- rentals/rental-model.js
+++ rentals/rental-model.js
@@ -3,5 +3,28 @@ const db = require('../database/dbConfig.js');
 module.exports = {
   find,
   findBy,
+  findById,
   insert
 };
+
+function find() {
+  return db('rentals');
+}
+
+function findBy(filter) {
+  return db('rentals').where(filter);
+}
+
+function findById(id) {
+  return db('rentals')
+    .where({ id })
+    .first();
+}
+
+function insert(rental) {
+  return db('rentals')
+    .insert(rental)
+    .then(() => {
+      return db('rentals');
+    });
+}
