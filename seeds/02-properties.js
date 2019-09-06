7d742e2df38baeea8a90d25b4d711123a6df69ed
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-05 19:43:21 -0700

    added new seeds

---
+++ seeds/02-properties.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('properties')
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex('properties').insert([
+        {
+          id: 1,
+          location: 'Miami',
+          price: 200,
+          description: 'By the Beach',
+          owner_id: 2
+        },
+        {
+          id: 2,
+          location: 'Denver',
+          price: 90,
+          description: 'Downtown Denver',
+          owner_id: 2
+        },
+        {
+          id: 3,
+          location: 'Portland',
+          price: 75,
+          description: 'downtown Portland',
+          owner_id: 4
+        }
+      ]);
+    });
+};
