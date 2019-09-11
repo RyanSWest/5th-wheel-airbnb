f3077e795981925b5ae3d25b331175bff4da1a60
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-10 21:15:40 -0700

    refactored rental validation middleware

--- rentals/rentalRouter.js
+++ rentals/rentalRouter.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const validateRentalId = require('../middleware/validateRentalId.js');
-const validateRenterType = require('../middleware/validateRenterType.js');
-const validatePropertyId = require('../middleware/validatePropertyId.js');
 const Rental = require('./rental-model.js');
 const Properties = require('../properties/prop-model.js');
 const User = require('../users/users-model.js');
@@ -127,61 +124,5 @@ async function validateRenterType(req, res, next) {
     res.status(500).json({ message: 'Error validating renter type' });
   }
 }
-
-//update reservation
-router.put(
-  '/reservation/:id',
-  validateRentalId,
-  validateRenterType,
-  validatePropertyId,
-  async (req, res) => {
-    const { id } = req.params;
-    try {
-      const rental = await Rental.update(id, req.body);
-      res.status(201).json(rental);
-    } catch (err) {
-      console.log(err);
-      res
-        .status(500)
-        .json({ message: 'There was an error updating reservation' });
-    }
-  }
-);
-
-//delete reservation
-
-router.delete('/:id', validateRentalId, async (req, res) => {
-  try {
-    const deleted = await Rental.remove(req.params.id);
-    console.log(deleted);
-    if (deleted == 1) {
-      const rentals = await Rental.find();
-      res.status(200).json(rentals);
-    } else {
-      res.status(400).json({ message: 'rental not found' });
-    }
-  } catch (err) {
-    res.status(500).json({ message: 'error deleting reservation' });
-  }
-});
-
-//Validate Rental Id exists
-// async function validateRentalId(req, res, next) {
-//   try {
-//     const rental = await Rental.findById(req.params.id);
-//     if (rental) {
-//       req.rental = rental;
-//       next();
-//     } else {
-//       res.status(404).json({ message: 'Rental Id not found' });
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: 'Error validating Rental' });
-//   }
-// }
-
-//validate renter_id belongs to a renter
-
-//validate land-
 
 module.exports = router;
