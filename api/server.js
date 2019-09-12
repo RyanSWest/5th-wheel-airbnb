bfcf2cf73e399e1618e820350cb1dcb7c6f6e789
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-12 15:01:07 -0700

    testing s3

--- api/server.js
+++ api/server.js
@@ -4,16 +4,28 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './upload');
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
+const aws = require('aws-sdk');
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  region: 'us-west-1'
 });
-const upload = multer({ storage: storage });
+
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    acl: 'public-read',
+    bucket: 'fifth-wheel',
+    key: function(req, file, cb) {
+      console.log(file);
+      cb(null, file.originalname);
+    }
+  })
+});
 
 const restricted = require('../auth/restricted-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -45,6 +57,7 @@ const server = express();
 // knexSessionStore(session);
 
 server.use(helmet());
+server.use(bodyParser.json());
 server.use(express.json());
 server.use(cors());
 server.use(session(sessionOptions));
@@ -58,12 +71,12 @@ server.get('/', (req, res) => {
   res.json({ api: 'Welcome to 5th Wheel Air B & B !' });
 });
 
-server.post('/single', upload.single('single'), (req, res) => {
-  try {
-    res.send(req.file);
-  } catch (err) {
-    res.send(400).statusMessage({ message: 'Error uploading photo' });
-  }
+server.get('/multer', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.post('/upload', upload.array('upl', 1), (req, res, next) => {
+  res.send('Uploaded!');
 });
 
 module.exports = server;
