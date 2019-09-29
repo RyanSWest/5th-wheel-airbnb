103b20ab75d5fca11d50c45fcdc8155a6d217aba
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-28 23:04:37 -0700

    knex update

--- knexfile.js
+++ knexfile.js
@@ -43,7 +43,10 @@ module.exports = {
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
     }
   }
 };
