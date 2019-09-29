e64d016f62760e31141b982486bc7e2dd54a7e7a
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-28 23:16:28 -0700

    fixing seeding

--- seeds/02-properties.js
+++ seeds/02-properties.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('properties')
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex('properties').insert([
