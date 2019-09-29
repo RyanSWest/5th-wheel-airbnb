9a33afbf1560deec69dfb0136ae6a6f9d714e5dd
Jeremy Morales <jeremy.morales@gmail.com>
2019-09-28 22:32:26 -0700

    testing postgres

--- knexfile.js
+++ knexfile.js
@@ -31,20 +31,19 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     useNullAsDefault: true,
 
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+    connection: process.env.DATABASE_URL,
+    seeds: {
+      directory: './seeds'
+    },
+    migrations: {
+      directory: './migrations'
     },
     pool: {
       min: 2,
       max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
     }
   }
 };
